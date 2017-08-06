2824a4405cafd95cba19d6c6a484676712599954
Re-z <re-z@ukr.net>
2017-08-06 16:38:56 +0300

    created skeleton

---
+++ build/js/common.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    //slider
+    $('.slider').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        focusOnSelect: true
+    });
+
+    ////mob script
+    //$('.nav-mob__holder').click(function(){
+    //    $(this).find('.nav-mob__box').toggle()
+    //});
+    //$('.burger').click(function(){
+    //    $('.nav-mob').addClass('is-visible')
+    //});
+    //$('.nav-mob__close').click(function(){
+    //    $('.nav-mob').removeClass('is-visible')
+    //});
+
+
+
+
+
+
+
+
+});
